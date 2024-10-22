5509949c4e0fb4e3979dab8d293f9a8ceb3407a7
Breyner012 <bhernandezos@cesde.net>
2024-10-21 22:53:54 -0500

    Resto de modificadores

--- DOM/script.js
+++ DOM/script.js
@@ -8,3 +8,35 @@ console.dir(input);
 
 // Atributos
 input.value = 'Apellido';
+
+// Modificando texto
+
+const titulo = document.querySelector("#app-title");
+console.log(titulo);
+console.dir(titulo);
+
+titulo.textContent = "Nuevo título";
+titulo.innerText = "otro título";
+
+// Modificando estilos
+
+const header = document.querySelector('header');
+console.log(header);
+
+header.style.backgroundColor = 'cadetblue';
+
+// Modificando clases
+const menu = document.querySelector('.menu');
+
+menu.className = 'main-menu';
+
+// Modificando visibilidad de elementos
+const button = document.querySelector('button');
+console.log(button);
+
+button.addEventListener('click', () => {
+    header.classList.toggle('invisible');
+});
+
+
+
