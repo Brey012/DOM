72612d71fc550712c4956a0145780bfba9392fc1
Breyner012 <bhernandezos@cesde.net>
2024-10-23 13:39:06 -0500

    Terminado la manipulacion de elementos

--- DOM/script.js
+++ DOM/script.js
@@ -1,42 +1,44 @@
+// const contentArea =  document.getElementById("contentArea");
 
-// Modificando atributos y propiedades
+// contentArea.innerHTML = "<p>Este es un nuevo párrafo</p>"
 
-const input = document.querySelector('input');
-console.log(input);
-
-console.dir(input);
-
-// Atributos
-input.value = 'Apellido';
-
-// Modificando texto
-
-const titulo = document.querySelector("#app-title");
-console.log(titulo);
-console.dir(titulo);
-
-titulo.textContent = "Nuevo título";
-titulo.innerText = "otro título";
-
-// Modificando estilos
-
-const header = document.querySelector('header');
-console.log(header);
-
-header.style.backgroundColor = 'cadetblue';
-
-// Modificando clases
-const menu = document.querySelector('.menu');
-
-menu.className = 'main-menu';
-
-// Modificando visibilidad de elementos
-const button = document.querySelector('button');
-console.log(button);
-
-button.addEventListener('click', () => {
-    header.classList.toggle('invisible');
-});
+// contentArea.insertAdjacentHTML("beforeend", "<p>Este es un otro nuevo párrafo</p>")
 
 
+const listArea = document.getElementById("listArea");
 
+// innerHTML es una propiedad que permite modificar el contenido HTML de un elemento HTML
+listArea.innerHTML += "<li>Item 5</li>";
+
+// insertAdjacentHTML es una función que permite insertar contenido HTML en un elemento HTML
+listArea.insertAdjacentHTML("beforeend", "<li>Item 6</li>");
+
+// Crear un nuevo elemento HTML
+const newPElement = document.createElement("p");
+newPElement.textContent = "Este es un nuevo párrafo creado con CreateElement";
+
+// Agregar el nuevo elemento HTML al contenido del elemento con id contentArea
+// contentArea.append(newPElement);
+
+// Eliminar el nuevo elemento HTML del contenido del elemento con id contentArea
+// contentArea.removeChild(newPElement);
+
+// Eliminar el primer elemento de la lista
+ const firstItem = document.querySelector("li");
+ firstItem.remove();
+
+// Eliminar el último elemento de la lista
+const lastItem = document.querySelector("ul");
+lastItem.removeChild(lastItem.lastElementChild);
+
+// Clonar elementos
+const contentArea = document.querySelector("#contentArea");
+const originalP = contentArea.querySelector("p");
+const clonedP = originalP.cloneNode(true);
+contentArea.append(clonedP);
+clonedP.textContent = "Este es un párrafo clonado";
+
+// Reemplazar elementos
+const list = document.querySelector("#listArea");
+const itemToReplace = listArea.children[2];
+itemToReplace.replaceWith(clonedP);
